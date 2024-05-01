c004243ed13b5ed4e1eff085548989ee915af34b
WinstonWolf007 <ubguertin@gmail.com>
2024-04-30 20:32:10 -0400

    Update test.ts file

--- src/test/script.ts
+++ src/test/script.ts
@@ -16,9 +16,6 @@ class Script {
     private mixer: THREE.AnimationMixer | undefined;    
     private attack_animation: THREE.AnimationAction | undefined;
 
-    private object3D: THREE.Object3D | undefined;
-
-
     constructor(
         private scene: THREE.Scene,
         private renderer: THREE.WebGLRenderer,
@@ -27,117 +24,30 @@ class Script {
         this.renderer.setClearColor(0xFFFFFF);
     }
 
-    // start() {
-        // this.loader.load("./assets/entity/zombie/animation/zombie@atack1.fbx", (fbx) => {
+    start() {
+        // this.loader.load("./assets/entity/zombie/animation/zombie@atack1.fbx", (fbx) => {   
     
-        //     const geometries: Array<THREE.BufferGeometry> = [];
-        //     fbx.traverse((child) => {
-        //         if (child instanceof THREE.Mesh && child.geometry instanceof THREE.BufferGeometry) {
-        //             geometries.push(child.geometry);
-        //         }
-        //     });
-        //     const geometry: THREE.BufferGeometry = mergeGeometries(geometries);
-    
-        //     const root_path = "./assets/entity/zombie/textures/";
-        //     const material: THREE.Material = new MaterialLoader({
-        //         map: root_path + `1/HIGH/1_Albedo.png`,
-        //         emissiveMap: root_path + `1/HIGH/1_Emission.png`,
-        //         roughnessMap: root_path + `1/HIGH/1_gloss.png`,
-        //         displacementMap: root_path + `1/HIGH/1_Height.png`,
-        //         metalnessMap: root_path + `1/HIGH/1_metalik marmoset.png`,
-        //         normalMap: root_path + `1/HIGH/1_Normal.png`,
-        //         aoMap: root_path + `1/HIGH/1_Occlusion.png`
-        //     }).material;
-    
-        //     this.mesh2 = new THREE.Mesh(geometry, material);
-        //     // this.mesh = new THREE.InstancedMesh(geometry, material, this.number_of_zombies);
-        //     // this.scene.add(this.mesh);
-        //     this.scene.add(this.mesh2);
-        //     this.is_loaded = true;
-
-        //     this.mesh2.scale.set(0.01, 0.01, 0.01);
-        //     this.mesh2.position.set(
+        //     fbx.scale.set(0.01, 0.01, 0.01);
+        //     fbx.position.set(
         //         Math.random() * this.range - (this.range / 2),
         //         0,
         //         Math.random() * this.range - (this.range / 2)
         //     );
     
-        //     // for (let i = 0; i < this.number_of_zombies; i++) {
-        //     //     this.zombie.scale.set(0.01, 0.01, 0.01);
-        //     //     this.zombie.position.set(
-        //             // Math.random() * this.range - (this.range / 2),
-        //             // 0,
-        //             // Math.random() * this.range - (this.range / 2)
-        //     //     );
-    
-        //         // this.zombie.updateMatrix();
-        //         // this.mesh.setMatrixAt(i, this.zombie.matrix);
-    
-        //         // Animation
-        //         const mixer = new THREE.AnimationMixer(fbx);
-        //         const action = mixer.clipAction(fbx.animations[0]);
-        //         action.play();
-        //         // console.log(action.isRunning())
-        //         this.mixers.push(mixer);
-        //         console.log(this.mixers)
-    
-        // //         console.log("Animation mixer created and animation played");
-        // //     }
+        //     this.mixer = new THREE.AnimationMixer(fbx);
+        //     this.attack_animation = this.mixer.clipAction(fbx.animations[0]);
+        //     this.attack_animation.play()
+        //     this.scene.add(fbx);
         // });
-    // }
 
-    // update() {
-        // for (let i = 0; i < this.number_of_zombies; i++) {
-        //     this.mesh.getMatrixAt(i, this.zombie.matrix);
-        //     this.zombie.matrix.decompose(this.zombie.position, this.zombie.quaternion, this.zombie.scale);
-        //     this.zombie.updateMatrix();
-        //     this.mesh.setMatrixAt(i, this.zombie.matrix);
-        // }
+        const THIS = this;
+        const loader = new FBXLoader();
 
-        // this.mesh.instanceMatrix.needsUpdate = true;
-    // }
-
-    start() {
-        // Load the FBX model
-        this.loader.load("./assets/entity/zombie/animation/zombie@atack1.fbx", (fbx) => {
-            // const geometries: Array<THREE.BufferGeometry> = [];
-
-            // fbx.traverse((child) => {
-            //     if (child instanceof THREE.Mesh && child.geometry instanceof THREE.BufferGeometry) {
-            //         geometries.push(child.geometry);
-            //     }
-            // });
-            // const geometry = mergeGeometries(geometries);
-    
-            // Load materials
-            // const root_path = "./assets/entity/zombie/textures/";
-            // const material = new MaterialLoader({
-            //     map: root_path + `1/HIGH/1_Albedo.png`,
-            //     emissiveMap: root_path + `1/HIGH/1_Emission.png`,
-            //     roughnessMap: root_path + `1/HIGH/1_gloss.png`,
-            //     displacementMap: root_path + `1/HIGH/1_Height.png`,
-            //     metalnessMap: root_path + `1/HIGH/1_metalik marmoset.png`,
-            //     normalMap: root_path + `1/HIGH/1_Normal.png`,
-            //     aoMap: root_path + `1/HIGH/1_Occlusion.png`
-            // }).material;
-    
-            // Create mesh
-            this.is_loaded = true;
-    
-            // Set initial scale and position
-            fbx.scale.set(0.01, 0.01, 0.01);
-            fbx.position.set(
-                Math.random() * this.range - (this.range / 2),
-                0,
-                Math.random() * this.range - (this.range / 2)
-            );
-    
-            // Create Animation Mixer and play animation
-            this.mixer = new THREE.AnimationMixer(fbx);
-            this.attack_animation = this.mixer.clipAction(fbx.animations[0]);
-            this.attack_animation.play()
-            this.scene.add(fbx);
+        // Chargement du modèle FBX
+        loader.load('./assets/entity/zombie/animation/zombie@atack1.fbx', function (object) {
+            console.log(object)
         });
+        
     }
     
 
